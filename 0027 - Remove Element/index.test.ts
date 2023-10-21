9baa9405de005b20f88c15256191e8dec45fa04b
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2023-10-20 23:09:38 -0300

    feature: adding solution for problem 0027.

---
+++ 0027 - Remove Element/index.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from "bun:test";
+
+import { removeElement } from ".";
+
+describe("Remove Element", () => {
+  test("should return 2 when given nums = [3, 2, 2, 3], val = 3", () => {
+    expect(removeElement([3, 2, 2, 3], 3)).toEqual(2);
+  });
+
+  test("should return 5 when given nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2", () => {
+    expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)).toEqual(5);
+  });
+});
