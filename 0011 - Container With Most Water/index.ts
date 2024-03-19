a7051c7e32340a752e4e5aa00e00094809ce71be
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2024-03-19 17:28:57 -0300

    adding solution for problem 0011.

---
+++ 0011 - Container With Most Water/index.ts
@@ -0,0 +1,21 @@
+const maxArea = (height: number[]): number => {
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * right - left;
+
+    maxArea = Math.max(maxArea, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
+};
+
+export { maxArea };
