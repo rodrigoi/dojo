d859abc9d2c678dc298b1ce456bdbf0d3a222969
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2024-12-04 00:29:01 -0300

    refactor: small refactor on day 1.

--- advent-of-code-2024/day-01/index.ts
+++ advent-of-code-2024/day-01/index.ts
@@ -9,18 +9,18 @@ const numbers = input
   });
 
 // Sort each column independently
-const column1 = numbers.map((pair) => pair[0]).sort((a, b) => a - b);
-const column2 = numbers.map((pair) => pair[1]).sort((a, b) => a - b);
+const leftList = numbers.map((pair) => pair[0]).sort((a, b) => a - b);
+const rightList = numbers.map((pair) => pair[1]).sort((a, b) => a - b);
 
 // Combine the sorted columns back into pairs if needed
-const sortedColumns = column1.map((num, index) => [num, column2[index]]);
+const sortedColumns = leftList.map((num, index) => [num, rightList[index]]);
 
 const totalDistance = sortedColumns.reduce((sum, pair) => {
   return sum + Math.abs(pair[0] - pair[1]);
 }, 0);
 
-const similarity = column1.reduce((sum, num) => {
-  return sum + Math.abs(num * column2.filter((n) => n === num).length);
+const similarity = leftList.reduce((sum, num) => {
+  return sum + Math.abs(num * rightList.filter((n) => n === num).length);
 }, 0);
 
 console.log("Total distance:", totalDistance);
