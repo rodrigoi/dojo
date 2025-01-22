2b7da764fdc36925272631296f098da129c7cb39
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2025-01-22 20:35:45 -0300

    feat: adding solution for day 7 part two.

--- advent-of-code-2024/day-07/index.test.ts
+++ advent-of-code-2024/day-07/index.test.ts
@@ -1,10 +1,11 @@
+import { describe, expect, test } from "bun:test";
 import {
   calculateCalibrationResults,
   getPossibleResults,
   isValidEquation,
+  operations,
   parseEquations,
 } from ".";
-import { describe, expect, test } from "bun:test";
 
 const input = `190: 10 19
 3267: 81 40 27
@@ -34,21 +35,23 @@ describe("Day 7: Bridge Repair", () => {
   });
 
   test("should validate if an equation is valid for equations with two values", () => {
-    expect(isValidEquation(190, [10, 19])).toBe(true);
-    expect(isValidEquation(190, [10, 20])).toBe(false);
+    expect(isValidEquation(operations)(190, [10, 19])).toBe(true);
+    expect(isValidEquation(operations)(190, [10, 20])).toBe(false);
   });
 
   test("should validate if an equation is valid for equations with n values", () => {
-    expect(isValidEquation(3267, [81, 40, 27])).toBe(true);
-    expect(isValidEquation(3267, [81, 40, 28])).toBe(false);
+    expect(isValidEquation(operations)(3267, [81, 40, 27])).toBe(true);
+    expect(isValidEquation(operations)(3267, [81, 40, 28])).toBe(false);
   });
 
   test("should get possible results for an equation with two values", () => {
-    expect(getPossibleResults([10, 19])).toEqual([29, 190]);
+    expect(getPossibleResults(operations)([10, 19])).toEqual([29, 190]);
   });
 
   test("should get possible results for an equation with three values", () => {
-    expect(getPossibleResults([81, 40, 27])).toEqual([148, 3267, 3267, 87480]);
+    expect(getPossibleResults(operations)([81, 40, 27])).toEqual([
+      148, 3267, 3267, 87480,
+    ]);
   });
 
   test("should calculate the calibration results", () => {
@@ -63,7 +66,26 @@ describe("Day 7: Bridge Repair", () => {
       { result: 21037, values: [9, 7, 18, 13] },
       { result: 292, values: [11, 6, 16, 20] },
     ];
-    const result = calculateCalibrationResults(input);
+    const result = calculateCalibrationResults(operations, input);
     expect(result).toBe(3749);
+  });
+
+  test("should calculate the calibration results with contatenation operator", () => {
+    const input = [
+      { result: 190, values: [10, 19] },
+      { result: 3267, values: [81, 40, 27] },
+      { result: 83, values: [17, 5] },
+      { result: 156, values: [15, 6] },
+      { result: 7290, values: [6, 8, 6, 15] },
+      { result: 161011, values: [16, 10, 13] },
+      { result: 192, values: [17, 8, 14] },
+      { result: 21037, values: [9, 7, 18, 13] },
+      { result: 292, values: [11, 6, 16, 20] },
+    ];
+    const result = calculateCalibrationResults(
+      [...operations, (a, b) => parseInt(`${a}${b}`)],
+      input
+    );
+    expect(result).toBe(11387);
   });
 });
--- advent-of-code-2024/day-07/index.ts
+++ advent-of-code-2024/day-07/index.ts
@@ -13,37 +13,42 @@ export const parseEquations = (input: string) => {
     .filter((equation) => equation !== null);
 };
 
-const operations = [
+export const operations = [
   (a: number, b: number) => a + b,
   (a: number, b: number) => a * b,
 ];
 
-export const isValidEquation = (result: number, values: number[]): boolean => {
-  const possibleResults = getPossibleResults(values);
-  return possibleResults.includes(result);
-};
+export const isValidEquation =
+  (operations: Array<(a: number, b: number) => number>) =>
+  (result: number, values: number[]): boolean => {
+    const possibleResults = getPossibleResults(operations)(values);
+    return possibleResults.includes(result);
+  };
 
-export const getPossibleResults = (values: number[]): Array<number> => {
-  const [a, b, ...rest] = values;
+export const getPossibleResults =
+  (operations: Array<(a: number, b: number) => number>) =>
+  (values: number[]): Array<number> => {
+    const [a, b, ...rest] = values;
 
-  if (rest.length === 0) {
-    return operations.map((operation) => operation(a, b));
-  }
+    if (rest.length === 0) {
+      return operations.map((operation) => operation(a, b));
+    }
 
-  return operations.flatMap((operation) => {
-    const partialResult = operation(a, b);
-    return getPossibleResults([partialResult, ...rest]);
-  });
-};
+    return operations.flatMap((operation) => {
+      const partialResult = operation(a, b);
+      return getPossibleResults(operations)([partialResult, ...rest]);
+    });
+  };
 
 export const calculateCalibrationResults = (
+  operations: Array<(a: number, b: number) => number>,
   equations: {
     result: number;
     values: number[];
   }[]
 ) => {
   return equations.reduce((acc, equation) => {
-    if (isValidEquation(equation.result, equation.values)) {
+    if (isValidEquation(operations)(equation.result, equation.values)) {
       return acc + equation.result;
     }
     return acc;
@@ -53,7 +58,14 @@ export const calculateCalibrationResults = (
 if (import.meta.main) {
   const input = await Bun.file("./advent-of-code-2024/day-07/input.txt").text();
   const equations = parseEquations(input);
-  const result = calculateCalibrationResults(equations);
+  const partOneResult = calculateCalibrationResults(operations, equations);
+  const partTwoResult = calculateCalibrationResults(
+    [...operations, (a, b) => parseInt(`${a}${b}`)],
+    equations
+  );
 
-  console.log(`The total calibration result is ${result}`);
+  console.log(`The total calibration result is ${partOneResult}`);
+  console.log(
+    `The total calibration result with concatenations is ${partTwoResult}`
+  );
 }
